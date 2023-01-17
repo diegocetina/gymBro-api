b23219423216b9e7fe8ffc086eb440c05d632813
Diego Cetina <diego.cetina@hotmail.es>
2023-01-17 12:41:20 -0500

    get and create working fine

--- src/controllers/exerciseController.js
+++ src/controllers/exerciseController.js
@@ -1,18 +1,36 @@
+const bodyParser = require("body-parser");
 const excerciseService = require("../services/exerciseService")
 
 const getAllExcercises = (req, res)=>{
     const allExcercises = excerciseService.getAllExcercises()
-    res.send("get all excercises");
+    res.send({ status: "todo en orden", data: allExcercises });
 }
 
 const getOneExcercise = (req, res)=>{
-    const excercise = excerciseService.getOneExcercise()
-    res.send("get one excercise");
+    const {
+        params: { excersiceId },
+    } = req;
+    if(!excersiceId){
+        return ;
+    }
+    
+    const excercise = excerciseService.getOneExcercise(excersiceId)
+    res.send({ status: "OK", data: excercise });
 }
 
 const createNewExcercise = (req, res)=>{
-    const createdExcercise = excerciseService.createNewExcercise()
-    res.send("create new excercise");
+    const { body } = req;
+    if(
+        !body.name 
+    ){
+        return;
+    }
+    const newExcercise = {
+        name: body.name,  
+    }
+
+    const createdExcercise = excerciseService.createNewExcercise(newExcercise)
+    res.status(201).send({ status: "ok", data: createdExcercise });
 }
 
 const updateOneExcercise = (req, res)=>{
--- src/services/exerciseService.js
+++ src/services/exerciseService.js
@@ -1,13 +1,25 @@
+const { v4: uuid } = require("uuid");
+const Excercise = require("../database/Excercise");
+
 const getAllExcercises = () => {
-    return ; 
+    const allExcercises = Excercise.getAllExcercises();
+    return allExcercises; 
 };
 
-const getOneExcercise = () => {
-    return ; 
+const getOneExcercise = (excerciseId) => {
+    const excercise = Excercise.getOneExcercise(excerciseId)
+    return excercise; 
 };
 
-const createNewExcercise = () => {
-    return ; 
+const createNewExcercise = (newExcercise) => {
+    const excerciseToInsert = {
+        id: uuid(),
+        ...newExcercise,
+        createdAt: new Date().toLocaleString("en-US", { timeZone: "UTC" }),
+        updatedAt: new Date().toLocaleString("en-US", { timeZone: "UTC" }),
+    };
+    const createdExcercise = Excercise.createNewExcercise(excerciseToInsert);
+    return createdExcercise; 
 };
 
 const updateOneExcercise = () => {
